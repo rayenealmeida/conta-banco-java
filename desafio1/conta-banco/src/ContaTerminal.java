e7e424f6b7e0f69c7ed633debd15fed091523491
almeidarayene <almeidarayene@gmail.com>
2023-11-22 16:51:19 -0300

    finish

--- desafio1/conta-banco/src/ContaTerminal.java
+++ desafio1/conta-banco/src/ContaTerminal.java
@@ -1,17 +1,24 @@
+import java.util.Scanner;
 
 public class  ContaTerminal {
     public static void main(String[] args) throws Exception {
-        double saldo=25;
-        double valorsSolicitado=18;
 
-        if(saldo > valorsSolicitado){
-            System.out.println(saldo - valorsSolicitado);
-        }
-        else if(saldo < valorsSolicitado){
-            System.out.println("saldo insuficiente");
-        }
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Digite seu nome: ");
+        String nomeCliente = scanner.next();
 
+        System.out.print("Digite o número da Agência: ");
+        String numeroAgencia = scanner.next();
+
+        System.out.print("Digite o número da sua Conta: ");
+        int numeroConta = scanner.nextInt();
+
+        System.out.print("Digite o saldo que deseja depositar: ");
+        double saldo = scanner.nextInt();
+
+        System.out.print("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + numeroAgencia + ", conta " + numeroConta + " e seu saldo " + saldo + " já está disponível para saque.");
+        
         //TODO: conhecer e importar a classe Scanner
 
         //Exivir as mensagens pro usuário
