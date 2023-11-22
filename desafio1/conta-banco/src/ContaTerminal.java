dc298222cc07e679ffb038bfa827ec0f28979feb
almeidarayene <almeidarayene@gmail.com>
2023-11-22 15:11:57 -0300

    conta

--- desafio1/conta-banco/src/ContaTerminal.java
+++ desafio1/conta-banco/src/ContaTerminal.java
@@ -1,5 +1,23 @@
+
 public class  ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double saldo=25;
+        double valorsSolicitado=18;
+
+        if(saldo > valorsSolicitado){
+            System.out.println(saldo - valorsSolicitado);
+        }
+        else if(saldo < valorsSolicitado){
+            System.out.println("saldo insuficiente");
+        }
+
+
+        //TODO: conhecer e importar a classe Scanner
+
+        //Exivir as mensagens pro usuário
+
+        //Obter pela classe Scanner os valores digitados no terminal
+
+        //Exibir a mensagem conta criada
     }
 }
